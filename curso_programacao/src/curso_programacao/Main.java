400a62f507a795c3146e480f1c5a687af4bc3e3a
MarceloHenrique666 <112137025+MarceloHenrique666@users.noreply.github.com>
2023-05-03 13:33:10 -0300

    Add files via upload

---
+++ curso_programacao/src/curso_programacao/Main.java
@@ -0,0 +1,44 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Entrer three numbers");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int higer = max(a, b, c);
+
+		showResult(higer);
+		
+		sc.close();
+
+	}
+	
+	public static int max(int x, int y, int z) {
+		int aux;
+		if (x > y && x > z) {
+			aux = x;
+		}	else if (y > z) {
+				aux = y;
+			}
+			else {
+				aux = z;
+			}
+		return aux;
+		}
+	
+	public static void showResult(int value) {
+		System.out.println("Higer = " + value);
+	}
+}
+
+
+
+
